29104937aff981e03a0021402e4ee2adc6d876f2
Daniel <790119+DanTheMan827@users.noreply.github.com>
2026-05-18 20:14:41 -0500

    DB migration

---
+++ src/Jellyfin.Database/Jellyfin.Database.Providers.Sqlite/Migrations/20260518190230_PlaybackTitle.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jellyfin.Database.Providers.Sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class PlaybackTitle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
